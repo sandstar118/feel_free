20a78e0d879956359ccbc6e3432ec06e0458e155
AO2324-00 <ao.2324.00@gmail.com>
2021-11-28 10:27:46 +0900

    場所の登録機能を実装

--- controllers/Registration.py
+++ controllers/Registration.py
@@ -1,7 +1,21 @@
-from flask import jsonify, request
+from flask import jsonify, request, make_response
 import sqlite3
 
 dbname = 'FeelFree.db'
 
 def registration(request):
-    return
+
+    body = request.json
+    
+    if(body is None):
+        return make_response('', 400)
+    if(not ('title' in body and  'address' in body and 'image_url' in body )):
+        return make_response('', 400)
+
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
+    cur.execute("INSERT INTO places (title, address, image_url) VALUES (:title, :address, :image_url)", (body['title'], body['address'], body['image_url']))
+    conn.commit()
+    conn.close()
+
+    return make_response('', 200)
