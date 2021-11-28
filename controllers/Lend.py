ee73ab31f90eca0ceb78e143c9c5a893d27bfb85
sandstar118 <94741827+sandstar118@users.noreply.github.com>
2021-11-28 10:34:08 +0900

    Update Lend.py

--- controllers/Lend.py
+++ controllers/Lend.py
@@ -4,4 +4,13 @@ import sqlite3
 dbname = 'FeelFree.db'
 
 def get_lend(id):
-    return
+    # 貸出状態の取得
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM lend")
+    data = []
+    for row in cur:
+        data.append({ "id":  row[0], "place_id": row[1], "begin_date": row[3], "end_date": row[4], "purpose":row[5]})
+    conn.close()
+    return jsonify(data)
+    
